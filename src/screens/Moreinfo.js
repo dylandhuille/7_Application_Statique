3132b7e58fbd60c28f829a5e1a12472eacff9d27
dhuille <dhuilledylan@gmail.com>
2022-06-30 12:11:08 +0200

    add alert

--- src/screens/Moreinfo.js
+++ src/screens/Moreinfo.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Alert } from 'react-native';
 //Ici nous importons les components Card, Title et Paragraph de react-native-paper
 import { Card, Button } from 'react-native-paper';
 
@@ -6,6 +6,48 @@ import { Card, Button } from 'react-native-paper';
 import data from '../data/data.json'
 
 export default function Moreinfo(index) {
+
+    const AlertSm = () =>
+        Alert.alert(
+            "Ajouter au panier",
+            data[index.route.params.itemId.index].name + " " + data[index.route.params.itemId.index].sm + "€",
+            [
+                {
+                    text: "Non",
+                    onPress: () => console.log("Non Pressed"),
+                    style: "cancel"
+                },
+                { text: "Oui", onPress: () => console.log("Oui Pressed") }
+            ]
+        );
+    const AlertMoy = () =>
+        Alert.alert(
+            "Ajouter au panier",
+            data[index.route.params.itemId.index].name + " " + data[index.route.params.itemId.index].moy + "€",
+            [
+                {
+                    text: "Non",
+                    onPress: () => console.log("Non Pressed"),
+                    style: "cancel"
+                },
+                { text: "Oui", onPress: () => console.log("Oui Pressed") }
+            ]
+        );
+    const AlertXl = () =>
+        Alert.alert(
+            "Ajouter au panier",
+            data[index.route.params.itemId.index].name + " " + data[index.route.params.itemId.index].xl + "€",
+            [
+                {
+                    text: "Non",
+                    onPress: () => console.log("Non Pressed"),
+                    style: "cancel"
+                },
+                { text: "Oui", onPress: () => console.log("Oui Pressed") }
+            ]
+        );
+
+
     return (
         <View style={styles.container}>
             <Text style={styles.titre}>{data[index.route.params.itemId.index].name}</Text>
@@ -18,17 +60,20 @@ export default function Moreinfo(index) {
             <View style={styles.buttoncontainer} >
                 <Button
                     mode="contained"
-                    color='#D60C67' >
+                    color='#D60C67'
+                    onPress={AlertSm} >
                     {data[index.route.params.itemId.index].sm}
                 </Button>
                 <Button
                     mode="contained"
-                    color='#D60C67'>
+                    color='#D60C67'
+                    onPress={AlertMoy}>
                     {data[index.route.params.itemId.index].moy}
                 </Button>
                 <Button
                     mode="contained"
-                    color='#D60C67'>
+                    color='#D60C67'
+                    onPress={AlertXl}>
                     {data[index.route.params.itemId.index].xl}
                 </Button>
             </View>
@@ -55,13 +100,19 @@ const styles = StyleSheet.create({
         alignItems: 'center',
     },
     titre: {
+        paddingLeft: 20,
         color: '#D60C67',
         fontSize: 30,
         fontWeight: "bold"
     },
     text: {
         padding: 10,
-
-    }
-
+        fontSize: 20,
+    },
+    image: {
+        width: 400,
+        height: 300,
+        paddingLeft: 10,
+    },
 });
+
